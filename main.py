1ce7cee153fa2fe9766c7661e28d71f3edab5e3e
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-11 18:42:11 +0500

    .choice()

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ number_of_names = len(names)
 random_choice = random.randint(0, number_of_names-1)
 person_who_will_pay = names[random_choice]
 print(person_who_will_pay + " is going to buy the meal today.")
+# random.choice() is a simpler method 
