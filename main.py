254bf04e62f6961271c884f4b738cb4a91f4b52d
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-11 18:40:23 +0500

    use split and len fn

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+number_of_names = len(names)
+random_choice = random.randint(0, number_of_names-1)
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today.")
